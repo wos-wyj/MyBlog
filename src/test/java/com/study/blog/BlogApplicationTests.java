05fed0402dcfd388d43562febe2c5db9fa06eefe
wos-wyj <1251675508@qq.com>
2020-08-31 18:19:02 +0800

    first commit

---
+++ src/test/java/com/study/blog/BlogApplicationTests.java
@@ -0,0 +1,14 @@
+package com.study.blog;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
